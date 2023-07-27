cc4e44087da1fff0519d30f89502bc4c80b7430f
Roger Yen <rogeryen@gmail.com>
2023-07-27 17:42:18 -0400

    rails: custom queries

--- rails/flix/app/models/movie.rb
+++ rails/flix/app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   def flop?
     self.total_gross < 225000000
   end
+
+  def self.released
+    Movie.where("released_on < ?", Time.now.utc).order(released_on: :desc)
+  end
 end
