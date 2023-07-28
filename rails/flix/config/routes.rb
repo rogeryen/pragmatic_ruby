da872fac8e5de38c2b81b281a4acfcdaf6cc7899
Roger Yen <rogeryen@gmail.com>
2023-07-28 16:47:56 -0400

    rails: one-to-many nested resources

--- rails/flix/config/routes.rb
+++ rails/flix/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "movies#index"
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 end
