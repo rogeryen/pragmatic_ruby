5163ad0ecf15d69d154a8157dfaf94fe3f7e06f5
Roger Yen <rogeryen@gmail.com>
2023-07-28 13:57:06 -0400

    rails: handling validation errors

--- rails/flix/app/controllers/movies_controller.rb
+++ rails/flix/app/controllers/movies_controller.rb
@@ -14,8 +14,12 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
   end
 
   def new
@@ -24,8 +28,12 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
